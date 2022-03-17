de0151a344690763f36b01264408c31585a5b68a
Lex-Llanes <95273812+Lex-Llanes@users.noreply.github.com>
2022-03-17 15:49:47 -0700

    getting through setting up server

--- Eventonica/server/routes/events.js
+++ Eventonica/server/routes/events.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Our Express is working correctly' });
+  res.render('index', { title: 'This is my events route' });
 });
 
 module.exports = router;
