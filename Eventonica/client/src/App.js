21af99221db883a52db0eaf7bd5d6c19768baa98
Lex-Llanes <95273812+Lex-Llanes@users.noreply.github.com>
2022-03-16 22:56:51 -0700

    Finished form fields

--- Eventonica/client/src/App.js
+++ Eventonica/client/src/App.js
@@ -1,12 +1,17 @@
 import React from "react"
 import "./App.css";
 import User from "./components/user"
+import Events from "./components/events"
 
 
 function App() {
   return (
     <div className="App">
      <User />
+     <br></br>
+     <br></br>
+     
+     <Events />
     </div>
   );
 }
