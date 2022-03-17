13b8f070760de9a1c2a0a48b474ca58654892790
Lex-Llanes <95273812+Lex-Llanes@users.noreply.github.com>
2022-03-17 15:18:23 -0700

    restarting server side

--- Eventonica/server/routes/events.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'This is my Events route' });
-});
-
-module.exports = router;
