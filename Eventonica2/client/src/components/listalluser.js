beaaf0397cea616af1373db1388514caf46b3c59
Lex-Llanes <95273812+Lex-Llanes@users.noreply.github.com>
2022-03-18 07:30:37 -0700

    saving

--- Eventonica2/client/src/components/listalluser.js
+++ Eventonica2/client/src/components/listalluser.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import EditUser from "./edituser"
 
 
 const ListAllUsers = (props) => {
@@ -6,14 +7,26 @@ const ListAllUsers = (props) => {
     const [userDetails, setUserDetails] = useState([])
 
 
+    //Delete user
+    async function deleteUser (id) {
+        try {
+            const response = await fetch (`http://localhost:5000/user/${id}`,{ method: "DELETE"})
+            setUserDetails(userDetails.filter(user => user.id !== id))
+        } catch (error) {
+            console.error(error.message)
+        }
+    }
 
+
+
+    //function to get out list of users data
     async function getAllUsers(){
         const response =  await fetch("http://localhost:5000/user");
         const listOfUsers = await response.json()
 
         setUserDetails(listOfUsers)
     }
-
+    //useEffect will constantly call getAllUsers() function
     useEffect(()=>{
         getAllUsers();
     }, []);
@@ -39,8 +52,12 @@ const ListAllUsers = (props) => {
                     <td>{user.id}</td>
                     <td>{user.name}</td>
                     <td>{user.email}</td>
-                    <td>Edit</td>
-                    <button className="btn btn-danger">Delete</button>
+                    <td> 
+                        <EditUser userDetails={user}/> 
+                    </td>
+                    <button 
+                        className="btn btn-danger"
+                        onClick={() => deleteUser(user.id)}                    >Delete</button>
                 </tr>
             ))}
 
